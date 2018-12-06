011b8392a3c45cf567b9f0013c4c5db331fea212
I_AM Elaine <elaine.onb@gmail.com>
2018-12-06 17:04:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,10 @@
 # Add  code here!
 def prime?(int)
-  if int <= 1 
+  if int == 1 || int == 0
     return false
-  end
-  if int <= 3
-    return true
-  end
+  else 
+    while i < int do
+      
     
-  if int % 2 == 0 || int % 3 == 0 
-    return false
-  else
-    return true
   end
 end
